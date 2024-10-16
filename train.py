0ea7d7bfab941bb6d88d78b02a5ffbb28c2335a8
Kukjun Lee <lxx3380@gmail.com>
2024-10-16 17:27:25 +0900

    fix: using train parameter
    hidden_size, num_fiducial

--- train.py
+++ train.py
@@ -274,13 +274,13 @@ if __name__ == '__main__':
     parser.add_argument('--SequenceModeling', type=str, required=True, help='SequenceModeling stage. None|BiLSTM')
     parser.add_argument('--Prediction', type=str, required=True, help='Prediction stage. CTC|Attn')
     # parser.add_argument('--num_fiducial', type=int, default=20, help='number of fiducial points of TPS-STN')
-    parser.add_argument('--num_fiducial', type=int, default=40, help='number of fiducial points of TPS-STN')
+    parser.add_argument('--num_fiducial', type=int, default=80, help='number of fiducial points of TPS-STN')
     parser.add_argument('--input_channel', type=int, default=1,
                         help='the number of input channel of Feature extractor')
     parser.add_argument('--output_channel', type=int, default=512,
                         help='the number of output channel of Feature extractor')
-    # parser.add_argument('--hidden_size', type=int, default=512, help='the size of the LSTM hidden state')
-    parser.add_argument('--hidden_size', type=int, default=1024, help='the size of the LSTM hidden state')
+    # parser.add_argument('--hidden_size', type=int, default=256, help='the size of the LSTM hidden state')
+    parser.add_argument('--hidden_size', type=int, default=512, help='the size of the LSTM hidden state')
 
     opt = parser.parse_args()
 
